5655148e7e07556ba33fa93ab3dec4c937879138
Alecx Moritz <hanelore@live.com>
2018-10-08 15:33:44 -0400

    initial commit

---
+++ src/components/mainComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class MainComponent extends Component {
+    render() {
+
+        console.log("test");
+
+        return(
+            <div>
+                <h2>This works</h2>
+                {/* navbar */}
+                {/* create deck button */}
+                {/* create card */}
+                {/* be quizzed by deck */}
+            </div>
+        )
+    }
+}
